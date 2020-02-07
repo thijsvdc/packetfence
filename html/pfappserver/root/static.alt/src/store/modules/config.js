fd8051039f41fd93619dccc9ae1dcd85481ebfd7
Darren Satkunas <darren@satkunas.com>
2020-02-07 14:46:23 -0500

    (web admin) fix loop when items is null

--- html/pfappserver/root/static.alt/src/store/modules/config.js
+++ html/pfappserver/root/static.alt/src/store/modules/config.js
@@ -1114,7 +1114,7 @@ const actions = {
       commit('PKI_CAS_REQUEST')
       return api.getPkiCas().then(response => {
         const { data: { items = [] } = {} } = response
-        commit('PKI_CAS_UPDATED', items)
+        commit('PKI_CAS_UPDATED', items || [])
         return state.pkiCas
       })
     } else {
@@ -1129,7 +1129,7 @@ const actions = {
       commit('PKI_PROFILES_REQUEST')
       return api.getPkiProfiles().then(response => {
         const { data: { items = [] } = {} } = response
-        commit('PKI_PROFILES_UPDATED', items)
+        commit('PKI_PROFILES_UPDATED', items || [])
         return state.pkiProfiles
       })
     } else {
@@ -1144,7 +1144,7 @@ const actions = {
       commit('PKI_CERTS_REQUEST')
       return api.getPkiCerts().then(response => {
         const { data: { items = [] } = {} } = response
-        commit('PKI_CERTS_UPDATED', items)
+        commit('PKI_CERTS_UPDATED', items || [])
         return state.pkiCerts
       })
     } else {
